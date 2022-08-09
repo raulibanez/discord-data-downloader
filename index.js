c27962631eddc16fac4e677ba12febf49e4fae72
Raúl Ibáñez <raulibaper@gmail.com>
2022-08-09 18:55:54 +0200

    first version

---
+++ index.js
@@ -0,0 +1,38 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', async () => {
+    const guild = await client.guilds.fetch("790498406333808680"); 
+
+    await guild.fetch();
+
+    let roleDict = {};
+    await guild.roles.fetch()
+        .then(roles => {
+            roles.forEach(role => {
+                roleDict[role.id] = role.name;
+            });
+        })
+        .catch(console.error);
+
+    console.log('id,nickname,bot,username,roles');   
+    await guild.members.fetch()
+        .then(members => {
+            members.forEach(member => {
+                const user = member.user;
+                const roles = member._roles.map(roleId => roleDict[roleId]);         
+                console.log([user.id, member.nickname, user.bot, user.username, roles.join('|')].join(','));	
+            });
+        })
+        .catch(console.error);
+
+    client.destroy();
+});
+
+// Login to Discord with your client's token
+client.login(token);
